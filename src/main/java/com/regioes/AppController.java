5d91b1914b0c6808be0996fb3ce394f4a91760f5
Yaskara Santangelo <yaskaraguimaraes@hotmail.com>
2025-04-21 22:36:16 -0300

    Altera post para get

--- src/main/java/com/regioes/AppController.java
+++ src/main/java/com/regioes/AppController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Map;
@@ -17,34 +18,35 @@ public class AppController {
         return Map.of("message", "Olá, mundo!");
     }
 
-    @PostMapping("/info_regiao")
-    public Map<String, String> info_regiao(@RequestBody InfoDTO info) {
+    @GetMapping("/info_regiao")
+    public Map<String, String> info_regiao(@RequestParam String regiao) {
         RegioesService regiao_service = new RegioesService();
-        RegiaoModel regiao = regiao_service.getRegiaoByName(info.nome);
+        RegiaoModel regiaoModel = regiao_service.getRegiaoByName(regiao);
         Map<String, String> infos = new HashMap<String, String>();
-
-        infos.put("nome", regiao.getNome());
-        infos.put("gases", regiao.getGases());
-        infos.put("lixos", regiao.getLixo());
-        infos.put("populacao", String.valueOf(regiao.getPopulacao()));
-
+        
+        if(regiaoModel != null) {
+	        infos.put("nome", regiaoModel.getNome());
+	        infos.put("gases", regiaoModel.getGases());
+	        infos.put("lixos", regiaoModel.getLixo());
+	        infos.put("populacao", String.valueOf(regiaoModel.getPopulacao()));
+        }
         return infos;
     }
-    @GetMapping("/{regiao_id}/questao")
-    public Map<String, Map<String, String>> questoes(@PathVariable int regiao_id) {
-        QuestoesService questoes_service = new QuestoesService();
-        ArrayList<QuestaoModel> questoes = questoes_service.getQuestoesByRegiao(regiao_id);
-        Map<String, Map<String, String>> questoes_json = new HashMap<String, Map<String, String>>();
-        for (int i = 0; i < questoes.size(); i++) {
-            Map<String, String> current_questao = new HashMap<String, String>();
-            current_questao.put("pergunta", questoes.get(i).getPergunta());
-            current_questao.put("alternativa", questoes.get(i).getAlternativasAsAString());
-            current_questao.put("questao_id", questoes.get(i).getIdString());
-            current_questao.put("regiao_id", questoes.get(i).getRegiaoIdString());
-
-            questoes_json.put(String.valueOf(i), current_questao);
-        }
-
-        return questoes_json;
-    }
+//    @GetMapping("/{regiao_id}/questao")
+//    public Map<String, Map<String, String>> questoes(@PathVariable int regiao_id) {
+//        QuestoesService questoes_service = new QuestoesService();
+//        ArrayList<QuestaoModel> questoes = questoes_service.getQuestoesByRegiao(regiao_id);
+//        Map<String, Map<String, String>> questoes_json = new HashMap<String, Map<String, String>>();
+//        for (int i = 0; i < questoes.size(); i++) {
+//            Map<String, String> current_questao = new HashMap<String, String>();
+//            current_questao.put("pergunta", questoes.get(i).getPergunta());
+//            current_questao.put("alternativa", questoes.get(i).getAlternativasAsAString());
+//            current_questao.put("questao_id", questoes.get(i).getIdString());
+//            current_questao.put("regiao_id", questoes.get(i).getRegiaoIdString());
+//
+//            questoes_json.put(String.valueOf(i), current_questao);
+//        }
+//
+//        return questoes_json;
+//    }
 }
